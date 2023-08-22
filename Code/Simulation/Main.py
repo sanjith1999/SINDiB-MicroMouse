b804c030f0e54f245ce1995fa49e7d0a155da61f
BiyonFernando <panthiyage.fernando@sydney.edu.au>
2023-08-22 09:55:23 +0530

    backtrack

--- Code/Simulation/Main.py
+++ Code/Simulation/Main.py
@@ -4,24 +4,24 @@ import sys
 x=0
 y=0
 orient=0
-debug = 1
+debug = 0
 
-cells = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+cells = [[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]]
 cell = 0
 flood=[[14,13,12,11,10,9,8,7,7,8,9,10,11,12,13,14],
         [13,12,11,10,9,8,7,6,6,7,8,9,10,11,12,13],
@@ -39,6 +39,25 @@ flood=[[14,13,12,11,10,9,8,7,7,8,9,10,11,12,13,14],
         [12,11,10,9,8,7,6,5,5,6,7,8,9,10,11,12],
         [13,12,11,10,9,8,7,6,6,7,8,9,10,11,12,13],
         [14,13,12,11,10,9,8,7,7,8,9,10,11,12,13,14]]
+
+backFlood=[[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+            [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]]
+
+
 '''
 cell
 refer walls in notebook
@@ -114,10 +133,15 @@ def updateWalls(x,y,orient,L,R,F):
             cells[y][x]= 1
         elif (orient==3): 
             cells[y][x]= 2
+    else:
+        cells[y][x]= 0
 
 def isAccessible(x,y,x1,y1):
     '''returns True if mouse can move to x1,y1 from x,y (two adjescent cells)
     '''
+    if(x1<0 or y1<0 or x1>=16 or y1>=16):
+        return False
+
     if (x==x1):
         if(y>y1):
             if(cells[y][x]==4 or cells[y][x]==5 or cells[y][x]==6 or cells[y][x]==10 or cells[y][x]==11 or cells[y][x]==12 or cells[y][x]==14 ):
@@ -433,49 +457,28 @@ def toMoveBack(x,y,xprev,yprev,orient):
     '''returns the direction to turn into L,F,R or B
     '''
     x0,y0,x1,y1,x2,y2,x3,y3 = getSurrounds(x,y)
-    val= flood[y][x]
-    prev=0
+    val= backFlood[y][x]
     minVals=[1000,1000,1000,1000]
 
     if (isAccessible(x,y,x0,y0)):
-        if (x0==xprev and y0==yprev):
-            prev=0
-        minVals[0]= flood[y0][x0]
+        minVals[0]= backFlood[y0][x0]
 
     if (isAccessible(x,y,x1,y1)):
-        if (x1==xprev and y1==yprev):
-            prev=1
-        minVals[1]= flood[y1][x1]
+        minVals[1]= backFlood[y1][x1]
 
     if (isAccessible(x,y,x2,y2)):
-        if (x2==xprev and y2==yprev):
-            prev=2
-        minVals[2]= flood[y2][x2]
+        minVals[2]= backFlood[y2][x2]
 
     if (isAccessible(x,y,x3,y3)):
-        if (x3==xprev and y3==yprev):
-            prev=3
-        minVals[3]= flood[y3][x3]
+        minVals[3]= backFlood[y3][x3]
 
-    maxVal=minVals[0]
+    minVal=1000
     minCell=0
-    noMovements=0
-    for i in minVals:
-        if (i!=1000):
-            noMovements+=1
 
     for i in range(4):
-        if (minVals[i]!=1000 and minVals[i]> maxVal):
-            if (noMovements==1):
-                minVal= minVals[i]
-                minCell= i
-
-            else:
-                if(i==prev):
-                    pass
-                else:
-                    minVal= minVals[i]
-                    minCell= i
+        if (minVals[i]!=1000  and minVals[i]!=-1 and minVals[i]< minVal ):
+            minVal= minVals[i]
+            minCell= i
 
     #return(minCell)
     if (minCell==orient):
@@ -495,6 +498,11 @@ def showFlood(xrun,yrun):
         for y in range(16):
             API.setText(x,y,str(flood[y][x]))
 
+def showBackFlood(xrun,yrun):
+    for x in range(16):
+        for y in range(16):
+            API.setText(x,y,str(backFlood[y][x]))
+
 def printFlood():
     for y in range(15,-1,-1):
         temp= ""
@@ -512,6 +520,70 @@ def printCell():
 
         log(temp)
 
+def backtrack():
+    x,y =0,0
+    backFlood[0][0] = 0
+    stack = []
+    stack.append((x,y))
+    distance  = 1
+
+    while True:
+        if debug:
+            stk = "dist: "+str(distance)+"  stack :"
+            for i in range(len(stack)):
+                stk+= " ("+str(stack[i][0])+","+str(stack[i][1])+")"
+            log(stk)
+
+        tempstack = []
+
+        if (len(stack)==0):
+            break
+
+        br = 0
+        for x,y in stack:
+            if (flood[y][x]==0):
+                br=1
+                break
+            
+            # x,y = stack.pop(0)
+            x0,y0,x1,y1,x2,y2,x3,y3 = getSurrounds(x,y)
+
+            if(x0>=0 and y0>=0 and cells[y0][x0]>=0 and backFlood[y0][x0]==-1):
+                if (isAccessible(x,y,x0,y0)):
+                    tempstack.append((x0,y0))
+                    backFlood[y0][x0] = distance
+        
+            if(x1>=0 and y1>=0 and cells[y1][x1]>=0 and backFlood[y1][x1]==-1):
+                if (isAccessible(x,y,x1,y1)):
+                    tempstack.append((x1,y1))
+                    backFlood[y1][x1] = distance
+
+            if(x2>=0 and y2>=0 and cells[y2][x2]>=0 and backFlood[y2][x2]==-1):
+                if (isAccessible(x,y,x2,y2)):
+                    tempstack.append((x2,y2))
+                    backFlood[y2][x2] = distance
+
+            if(x3>=0 and y3>=0 and cells[y3][x3]>=0 and backFlood[y3][x3]==-1):
+                if (isAccessible(x,y,x3,y3)):
+                    tempstack.append((x3,y3))
+                    backFlood[y3][x3] = distance
+                    
+        if debug:
+            log("backFloor::::::::::")
+            for y in range(15,-1,-1):
+                temp= ""
+                for x in range(16):
+                    temp+= str(backFlood[y][x]).ljust(3)
+
+                log(temp)    
+        
+        if br:
+            break
+        
+        stack = tempstack
+        distance+=1
+    
+
 
 def main():
     x=0
@@ -519,6 +591,15 @@ def main():
     xprev=0
     yprev=0
     orient=0
+    '''
+        orients :
+            0- North
+            1- East
+            2- South
+            3- West
+    '''
+
+    state = 0
 
     while True:
 
@@ -529,19 +610,38 @@ def main():
 
         if debug : log("#####      "+str(flood[y][x])+"            #####")
 
-        if (flood[y][x]>=1):
-            floodFill(x,y,xprev,yprev)
-            if debug : printFlood()
+        if state==0:
             
-        else:
-            printCell()
-            while(True):
-                pass
+            if (flood[y][x]>=1):
+                floodFill(x,y,xprev,yprev)
+                if debug : printFlood()
+                direction= toMove(x,y,xprev,yprev,orient)
+            else:
+                state = 1
+                printCell()
+                # direction = 'B'
+                backtrack()
+                direction= toMoveBack(x,y,xprev,yprev,orient)
+                # for i in range(1000):
+                #     pass
+                # while(True):
+                #     pass
+            showFlood(x,y)
 
-                
+        elif state == 1:
+            
+            direction= toMoveBack(x,y,xprev,yprev,orient)
 
-        direction= toMove(x,y,xprev,yprev,orient)
+            if (backFlood[y][x]==0):
+                while True:
+                    pass
 
+            showBackFlood(x,y)
+
+
+
+
+            
         
         if (direction=='L'):
             API.turnLeft()
@@ -558,8 +658,8 @@ def main():
             orient = API.orientation(orient,'L')
 
 
-        log("moveForward")
-        showFlood(x,y)
+        # log("moveForward")
+        
         API.moveForward()
         xprev=x
         yprev=y
