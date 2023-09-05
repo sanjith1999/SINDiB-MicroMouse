e458b865f1260dc740fa426aa914864f3760b17d
ishrath99 <ishrathahamed0@gmail.com>
2023-09-05 23:35:28 +0530

    Added: states.c states.h

---
+++ Code/SINDiB_Firmware/Core/Inc/states.h
@@ -0,0 +1,9 @@
+#ifndef STATES_H
+#define STATES_H
+
+// Function prototypes
+void SearchRun(void);
+void FastRun(void);
+void AdjustSpeed(void);
+
+#endif /* STATES_H */
--- Code/SINDiB_Firmware/Core/Src/main.c
+++ Code/SINDiB_Firmware/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "states.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
